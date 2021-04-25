7cc3ea7c10090373bd7e8d53d4c22d6e07c13d23
LukasBarton <62757283+LukasBarton@users.noreply.github.com>
2021-04-25 19:16:12 +0200

    mam

--- src/main/java/cz/educanet/praha/MostAverageNumber.java
+++ src/main/java/cz/educanet/praha/MostAverageNumber.java
@@ -5,6 +5,19 @@ import java.util.Scanner;
 public class MostAverageNumber {
 
     static int findMostAverageNumber(Scanner sc) {
-        return 0;
+       double total = 0;
+       int count = 0;
+       int average = 0;
+       while (sc.hasNext()){
+           int c = sc.nextInt();
+           total = total + c;
+           count++;
+           if(count > 0){
+               average = (int) total/count;
+           }
+       }
+
+        return average;
     }
 }
+
