b7adefa42762ec11ddd3f5673dfa8083e540f20c
LukasBarton <62757283+LukasBarton@users.noreply.github.com>
2021-04-26 14:46:08 +0200

    mam

--- src/main/java/cz/educanet/praha/StringWithMostOccurrencesOfLetter.java
+++ src/main/java/cz/educanet/praha/StringWithMostOccurrencesOfLetter.java
@@ -3,6 +3,42 @@ package cz.educanet.praha;
 public class StringWithMostOccurrencesOfLetter {
 
     public static String findStringWithMostOccurrencesOfLetter(String[] strings, char letter) {
-        return "";
+        int maxCount = 0;
+        String maxString = null;
+        for(int i = 0; i < strings.length;i++){
+            String string = strings[i];
+            int counter = 0;
+            for (int j = 0; j < string.length(); j++) {
+                char c = string.charAt(j);
+                if(c == letter){
+                    counter++;
+                }
+            }if(counter > maxCount ){
+                maxCount = counter;
+                maxString = string;
+            }
+         }
+        return maxString;
     }
 }
+
+/*
+bautobus
+postupne projizdim  slovo a kdyz se pismeno = a prictu
+
+mam promenou counter
+zacnu projizdet slovo
+    kdyz se pismeno rovna a prictu ke counteru
+
+
+Vytvořím si proměnnou pro největší počet.
+Vytvořím si proměnnou pro slovo s největším počtem.
+Projedu slova
+    spočítám počet a
+    pokud je počet větší než největší počet,
+        uložím si ho jako největší počet
+        a uložím si to slovo jako slovo s nevětším počtem hledaných písmen
+
+
+
+ */
