42272fbd8ef18568bccef290917c1e63ae557f32
LukasBarton <62757283+LukasBarton@users.noreply.github.com>
2021-05-03 13:47:24 +0200

    mam

--- src/main/java/cz/educanet/praha/SchoolCountAllStudents.java
+++ src/main/java/cz/educanet/praha/SchoolCountAllStudents.java
@@ -5,6 +5,11 @@ import cz.educanet.praha.school.School;
 public class SchoolCountAllStudents {
 
     public static int countAllStudents(School school) {
-        return 0;
+        int r = 0;
+        for (int i = 0; i < school.classes.length;i++){
+            r = r + school.classes.length + i;
+        }
+
+        return r;
     }
 }
